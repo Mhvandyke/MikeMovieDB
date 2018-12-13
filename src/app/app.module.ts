5601025b201662a8c6df917501cfd9e38ffd23e7
Nebuchadnezzar <n3bu@babylon.rev.182>
2018-12-13 15:55:42 -0500

    added basic firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +10,12 @@ import {Routes, RouterModule} from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ReserveComponent} from './reserve/reserve.component';
-
+import { PostComponent } from './post/post.component';
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'reserve', component: ReserveComponent}
+  {path: 'reserve', component: ReserveComponent},
+   {path: 'post', component: PostComponent}
 ];
 
 
@@ -22,12 +25,14 @@ const routes: Routes = [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    ReserveComponent 
+    ReserveComponent,
+    PostComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
